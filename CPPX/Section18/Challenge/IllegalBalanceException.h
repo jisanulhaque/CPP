72a8665dce26b8a2296448b6baef821aca9d1d24
JisanulHaque <jisanulhaque1996@gmail.com>
2024-03-12 15:35:27 +0600

    section challange on progress

---
+++ CPPX/Section18/Challenge/IllegalBalanceException.h
@@ -0,0 +1,15 @@
+#ifndef __ILLEGAL_BALANCE_EXCEPTION_H__
+#define __ILLEGAL_BALANCE_EXCEPTION_H__
+#include <iostream>
+
+class IllegalBalanceException: public std::exception
+{
+public:
+    IllegalBalanceException() {}
+    ~IllegalBalanceException() {}
+    virtual const char* what() const noexcept{
+        return "Illegal balance exception";
+    }
+};
+
+#endif // __ILLEGAL_BALANCE_EXCEPTION_H__
