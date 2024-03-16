1be0e5600adbe946b89a7c86ad26bc12661fcf4f
JisanulHaque <jisanulhaque1996@gmail.com>
2024-03-16 16:02:27 +0600

    boolean manipulators added

--- CPPX/Section19/manip_boolean/main.cpp
+++ CPPX/Section19/manip_boolean/main.cpp
@@ -4,7 +4,30 @@
 using namespace std;
 
 int main(){
+    cout<<"noboolalpha- default (10==10): "<<(10==10)<<endl; //0
+    cout<<"noboolalpha- default (10==10): "<<(10==20)<<endl; //1
+    //set to true/false formatting
+    cout<<boolalpha;
+    cout<<"boolalpha default (10==10): "<<(10==10)<<endl; //true
+    cout<<"boolalpha- default (10==10): "<<(10==20)<<endl; //false
 
+    //setting still stays for boolean insertions
+    cout<<"boolalpha default (10==10): "<<(10==10)<<endl; //true
+    cout<<"boolalpha- default (10==10): "<<(10==20)<<endl; //false
 
+    //toggle to 0/1
+    cout<<noboolalpha;
+    cout<<"noboolalpha- default (10==10): "<<(10==10)<<endl; //0
+    cout<<"noboolalpha- default (10==10): "<<(10==20)<<endl; //1
+
+    //set back to true/false using setf method
+    cout.setf(std::ios::boolalpha);
+    cout<<"boolalpha default (10==10): "<<(10==10)<<endl; //true
+    cout<<"boolalpha- default (10==10): "<<(10==20)<<endl; //false
+
+    //reset to default which is 0/1
+    cout<<std::resetiosflags(std::ios::boolalpha);
+    cout<<"default (10==10): "<<(10==10)<<endl; //0
+    cout<<"default (10==10): "<<(10==20)<<endl; //1
     return 0;
 }
